f41ff4a88c12862150f12d1ed817a6356b686a9d
Stavros Labrinos <s.labrinos@neurocom.gr>
2025-10-04 19:58:10 +0300

    feat: integrate OIDC authentication
    Added `oidc-client-ts` and `react-oidc-context` dependencies for managing OIDC-based authentication. Configured `oidc.ts` with user manager settings and integrated `AuthProvider` into the app's main render function to support authentication flows.

---
+++ src/config/oidc.ts
@@ -0,0 +1,38 @@
+import { UserManager, WebStorageStateStore } from "oidc-client-ts";
+
+const getUILocale = () => {
+  const language = localStorage.getItem("i18nextLng") || "en";
+  const localeMap: Record<string, string> = {
+    "en-US": "en",
+    en: "en",
+    el: "el",
+    "en-GR": "el",
+  };
+  return localeMap[language] || "en";
+};
+
+const oidcConfig = new UserManager({
+  authority: import.meta.env.VITE_AUTHORITY,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  redirect_uri: `${window.location.origin}${window.location.pathname}`,
+  post_logout_redirect_uri: window.location.origin,
+  userStore: new WebStorageStateStore({ store: window.sessionStorage }),
+  response_type: "code",
+  response_mode: "query",
+  loadUserInfo: true,
+  monitorSession: true,
+  scope: "openid email profile",
+  automaticSilentRenew: true,
+  silentRequestTimeoutInSeconds: 10,
+  ui_locales: getUILocale(),
+});
+
+const onSigninCallback = () => {
+  window.history.replaceState({}, document.title, window.location.pathname);
+};
+
+const onSignoutCallback = () => {
+  window.history.replaceState({}, document.title, window.location.pathname);
+};
+
+export const oidc = { oidcConfig, onSigninCallback, onSignoutCallback };
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { store } from "@/store";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router";
 import { router } from "@/components/navigation/router.tsx";
+import { AuthProvider } from "react-oidc-context";
+import { oidc } from "@/config/oidc.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <AuthProvider {...oidc}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </AuthProvider>
   </StrictMode>,
 );
